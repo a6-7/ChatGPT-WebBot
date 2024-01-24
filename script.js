f6f1f83964daad1bdf7587fff7ee98e6b7c5c7bc
a6-7 <147426290+a6-7@users.noreply.github.com>
2024-01-24 13:52:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("send-btn").addEventListener("click", function () {
+  sendMessageToAI();
+});
+
+function sendMessageToAI() {
+  let userInput = document.getElementById("user-input").value;
+
+  let requestBody = {
+    model: "gpt-3.5-turbo-1106",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: userInput }
+    ],
+    temperature: 1,
+  };
+
+  let requestHeaders = {
+    Authorization: "Bearer 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",
+  };
+
+  axios
+    .post("https://api.openai.com/v1/chat/completions", requestBody, {
+      headers: requestHeaders,
+    })
+    .then((response) => {
+      let reply = response.data.choices[0].message.content;
+      document.getElementById("text1").textContent = reply;
+    })
+    .catch((error) => {
+      console.error("Error ssnding request to AI:", error);
+
+    });
+}
